1da941bfb021f694841628b7bc6bbaf4944601eb
Nicolas Guitton <nicolas.guitton@nukesuite.com>
2017-10-06 18:18:51 +0200

    add postfix and send mail

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # get-Light-Novel
 It's a project for get light novel and transform it into ebook ready to be read.
+
+
+# Documentation :
+https://www.ibm.com/developerworks/xml/tutorials/x-epubtut/index.html
--- src/main/java/getLn/Controller/MangaController.java
+++ src/main/java/getLn/Controller/MangaController.java
@@ -1,5 +1,16 @@
 package getLn.Controller;
 
+import java.util.Properties;
+
+import javax.inject.Inject;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +20,9 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class MangaController {
+
+    @Inject
+    private Environment environment;
 
     /**
      * List reviewers.
@@ -32,6 +46,49 @@ public class MangaController {
         // @formatter:on
 
         return "pascoucocu";
+    }
+
+    private void sendMail() {
+        // Recipient's email ID needs to be mentioned.
+        final String to = "nic.guitton@gmail.com";
+
+        // Sender's email ID needs to be mentioned
+        final String from = "nic.guitton@gmail.com";
+
+        // Assuming you are sending email from localhost
+        final String host = environment.getProperty("postfix.url");
+
+        // Get system properties
+        final Properties properties = System.getProperties();
+
+        // Setup mail server
+        properties.setProperty("mail.smtp.host", host);
+
+        // Get the default Session object.
+        final Session session = Session.getDefaultInstance(properties);
+
+        try {
+            // Create a default MimeMessage object.
+            final MimeMessage message = new MimeMessage(session);
+
+            // Set From: header field of the header.
+            message.setFrom(new InternetAddress(from));
+
+            // Set To: header field of the header.
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+
+            // Set Subject: header field
+            message.setSubject("This is the Subject Line!");
+
+            // Now set the actual message
+            message.setText("This is actual message");
+
+            // Send message
+            Transport.send(message);
+            System.out.println("Sent message successfully....");
+        } catch (final MessagingException mex) {
+            mex.printStackTrace();
+        }
     }
 
 }
