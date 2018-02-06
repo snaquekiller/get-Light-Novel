2fe506f20f1093f9e2b4f871c663e825f7fd20bf
Nicolas Guitton <nicolas.guitton@nukesuite.com>
2018-02-06 19:05:01 +0100

    delta

--- server/get-ln-data/delta.sql
+++ server/get-ln-data/delta.sql
@@ -1,64 +1,82 @@
 CREATE TABLE manga
 (
-  id INT PRIMARY KEY AUTO_INCREMENT,
-  name VARCHAR(200),
-  author VARCHAR(100),
-  notes VARCHAR(100),
-  url VARCHAR(1000),
-  type VARCHAR(100),
+  id            BIGINT(20) UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+  name          VARCHAR(200),
+  author        VARCHAR(100),
+  notes         VARCHAR(100),
+  url           VARCHAR(1000),
+  type          ENUM ('LIGHT_NOVEL', 'MANGA'),
   creation_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
-  update_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
-  deleted TINYINT(1) DEFAULT 0
+  update_date   TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+  deleted       TINYINT(1)                      DEFAULT 0
 );
 
 CREATE TABLE manga_out
 (
-  id INT PRIMARY KEY AUTO_INCREMENT,
-  id_manga INT NOT NULL,
-  hours INT NOT NULL,
-  minutes INT NOT NULL,
-  secondes INT NOT NULL,
-  days INT,
+  id            BIGINT(20) UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+  manga_id      BIGINT(20) UNSIGNED                 NOT NULL,
+  hours         INT UNSIGNED                        NOT NULL,
+  minutes       INT UNSIGNED                        NOT NULL,
+  secondes      INT UNSIGNED                        NOT NULL,
+  days          INT,
+  nb_try        INT UNSIGNED,
+  status        ENUM ('IN_PROGRESS', 'FAILED', 'RE_TRY', 'AVAILABLE'),
   creation_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
-  update_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
-  deleted TINYINT(1) DEFAULT 0
+  update_date   TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+  deleted       TINYINT(1)                      DEFAULT 0
 );
 
 CREATE TABLE chapter
 (
-  id INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  text VARCHAR(10000),
-  num INT(11),
-  title VARCHAR(1000),
-  creation_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
-  update_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
-  tome INT(11),
-  file_id INT(11),
-  deleted TINYINT(1) DEFAULT 0
+  id            BIGINT(20) UNSIGNED PRIMARY KEY                 NOT NULL AUTO_INCREMENT,
+  text          VARCHAR(10000),
+  num           INT(11),
+  title         VARCHAR(1000),
+  creation_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP             NOT NULL,
+  update_date   TIMESTAMP DEFAULT CURRENT_TIMESTAMP             NOT NULL,
+  tome          INT(11),
+  file_id       INT(11),
+  deleted       TINYINT(1)                                               DEFAULT 0
 );
 
 
 CREATE TABLE file
 (
-  id INT PRIMARY KEY AUTO_INCREMENT,
-  name VARCHAR(1000),
-  type VARCHAR(100),
-  url VARCHAR(1000),
+  id            BIGINT(20) UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+  name          VARCHAR(1000),
+  type          VARCHAR(100),
+  url           VARCHAR(1000),
   creation_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
-  update_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
-  deleted TINYINT(1) DEFAULT 0
+  update_date   TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+  deleted       TINYINT(1)                      DEFAULT 0
 );
 
 CREATE TABLE user
 (
-  id INT PRIMARY KEY AUTO_INCREMENT,
-  email VARCHAR(1000),
-  pseudo VARCHAR(100),
-  nom VARCHAR(1000),
-  prenom VARCHAR(1000),
+  id            BIGINT(20) UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+  email         VARCHAR(1000),
+  pseudo        VARCHAR(100),
+  nom           VARCHAR(1000),
+  prenom        VARCHAR(1000),
   creation_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
-  update_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
-  deleted TINYINT(1) DEFAULT 0
+  update_date   TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+  deleted       TINYINT(1)                      DEFAULT 0
 );
+
+CREATE TABLE manga_subscription
+(
+  id            BIGINT(20) UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+  user_id       VARCHAR(1000),
+  manga_id      VARCHAR(100),
+  num_chapter   INT UNSIGNED,
+  creation_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+  update_date   TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+  deleted       TINYINT(1)                      DEFAULT 0
+);
+
 INSERT INTO LN.user (email, pseudo, nom, prenom) VALUES ('nic.guitton@gmail.com', 'snaquekiller', 'guitton', 'nicolas
 ');
+INSERT INTO LN.manga (name, author, notes, url, type, creation_date, update_date, deleted) VALUES ('
+Douluo Dalu 3 - Dragon King''s Legend', 'Tang Jia San Shao', NULL, NULL, 'LIGHT_NOVEL', '2018-02-06 17:56:35',
+                                                                                                   '2018-02-06 17:56:35', 0);
+
--- server/get-ln-data/src/main/java/getln/data/entity/MangaSubscription.java
+++ server/get-ln-data/src/main/java/getln/data/entity/MangaSubscription.java
@@ -1,5 +1,6 @@
 package getln.data.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
@@ -28,23 +29,66 @@ public class MangaSubscription extends AbstractDeletableJpaEntity<Long> {
     @JoinColumn(name = "manga_id")
     private Manga manga;
 
-    public User getUser() {
-        return user;
-    }
-
-    public void setUser(final User user) {
-        this.user = user;
-    }
-
-    public Manga getManga() {
-        return manga;
-    }
-
-    public void setManga(final Manga manga) {
-        this.manga = manga;
-    }
+    /**
+     * The number of the chapter since you subscribe
+     */
+    @Column(name = "num_chapter")
+    private int numChapter;
 
     public MangaSubscription() {
     }
 
+    /**
+     * Sets new The number of the chapter since you subscribe.
+     *
+     * @param numChapter New value of The number of the chapter since you subscribe.
+     */
+    public void setNumChapter(final int numChapter) {
+        this.numChapter = numChapter;
+    }
+
+    /**
+     * Gets The number of the chapter since you subscribe.
+     *
+     * @return Value of The number of the chapter since you subscribe.
+     */
+    public int getNumChapter() {
+        return numChapter;
+    }
+
+    /**
+     * Gets The manga.
+     *
+     * @return Value of The manga.
+     */
+    public Manga getManga() {
+        return manga;
+    }
+
+    /**
+     * Gets The user who subscript.
+     *
+     * @return Value of The user who subscript.
+     */
+    public User getUser() {
+        return user;
+    }
+
+    /**
+     * Sets new The user who subscript.
+     *
+     * @param user New value of The user who subscript.
+     */
+    public void setUser(final User user) {
+        this.user = user;
+    }
+
+    /**
+     * Sets new The manga.
+     *
+     * @param manga New value of The manga.
+     */
+    public void setManga(final Manga manga) {
+        this.manga = manga;
+    }
 }
