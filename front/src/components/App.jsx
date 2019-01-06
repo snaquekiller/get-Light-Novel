875596b681684ded1ec45e42f8094f47d1016665
Aenie <snaquekiller@hotmail.fr>
2019-01-06 19:57:50 +0100

    [DEV] Overlays on Connect button

--- front/src/components/App.jsx
+++ front/src/components/App.jsx
@@ -9,6 +9,7 @@ import Image from "react-bootstrap/lib/Image";
 import ControlLabel from "react-bootstrap/lib/ControlLabel";
 import Panel from "react-bootstrap/lib/Panel";
 import styled from "styled-components";
+import { Modal } from "react-overlays"
 
 import MangaService from "../service/MangaService.jsx";
 
@@ -44,6 +45,36 @@ const Manga = styled.div`
   }
 `;
 
+const backdropStyle = {
+  position: 'fixed',
+  zIndex: 1040,
+  top: 0,
+  bottom: 0,
+  left: 0,
+  right: 0,
+  backgroundColor: '#000',
+  opacity: 0.5
+};
+
+const modalStyle = function() {
+  // we use some psuedo random coords so nested modals
+  // don't sit right on top of each other.
+  let top = 50;
+  let left = 50;
+
+  return {
+    position: 'fixed',
+    width: 400,
+    zIndex: 1040,
+    top: top + '%',
+    left: left + '%',
+    border: '1px solid #e5e5e5',
+    backgroundColor: 'white',
+    boxShadow: '0 5px 15px rgba(0,0,0,.5)',
+    padding: 20
+  };
+};
+
 const PanelForm = styled.div`
   width: 400px;
   margin-left: auto;
@@ -54,14 +85,27 @@ export default class App extends React.Component {
     super(props);
     this.state = {
       manga: undefined,
-      url: ""
+      url: "",
+      showModal: false
     };
     this.scrap = this.scrap.bind(this);
     this.handleChange = this.handleChange.bind(this);
+
+    this.close = () => {
+      this.setState({ showModal: false });
+    };
+
+    this.open = () => {
+      this.setState({ showModal: true });
+    };
   }
 
   handleChange(e) {
     this.setState({ url: e.target.value });
+  }
+
+  renderBackdrop(props) {
+    return <div {...props} style={backdropStyle} />;
   }
 
   scrap() {
@@ -82,6 +126,21 @@ export default class App extends React.Component {
     return (
       <div style={{ textAlign: "center" }}>
         <h1>Welcome to the Send To Reader</h1>
+        <Button bsStyle="primary" onClick={this.open}>Connect</Button>
+          <Modal
+            onHide={this.close}
+            style={modalStyle()}
+            aria-labelledby="modal-label"
+            show={this.state.showModal}
+            renderBackdrop={this.renderBackdrop}
+            >
+          <div>
+            <h4 id="modal-label">Text in a modal</h4>
+            <p>
+              Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+            </p>
+          </div>
+        </Modal>
         <PanelForm>
           <Panel>
             <Form>
